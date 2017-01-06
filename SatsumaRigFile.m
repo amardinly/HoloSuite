7bdcb66340f3520c104582d4b44b0c2a8168c0c0
amardinly <alan.mardinly@gmail.com>
2017-01-06 13:05:53 -0800

    Updated HI3, Rig File

--- SatsumaRigFile.m
+++ SatsumaRigFile.m
@@ -1,13 +1,13 @@
 function locations = SatsumaRigFile();
-% Version 1.0
-% Edited by Alan Mardinly 8/22/16
+% Version 1.1
+% Edited by Alan Mardinly 1/6/17
 % IF YOU CHANGE THIS FILE, UPDATE THE VERSION AND ENSURE COPIES ARE PRESENT
 % AND SYNCHED ON ALL RELEVANT COMPUTERS!  THIS FILE MUST BE IN THE PATH FOR
 % ALL ELEMENTS OF HOLOSUITE
 
 locations.HoloRequest_DAQ='Z:\holography\SatsumaRig\HoloRequest-DAQ\';
 locations.HoloRequest_DAQ_PrintedHolo='Z:\holography\SatsumaRig\HoloRequest-DAQ\PrintedHolo\';
-locations.PowerCalib = 'Z:\holography\Calibration Parameters\20X_Objective_Calibration_LaserPower_WithMap.mat';
+locations.PowerCalib = 'Z:\holography\Calibration Parameters\20X_Objective_Calibration_LaserPower.mat';
 locations.savePath='C:\data\';
 locations.ROIanalysisDefaultPath = 'Z:\';
 locations.customHolo = 'Z:\holography\SatsumaRig\CustomHolo\';
