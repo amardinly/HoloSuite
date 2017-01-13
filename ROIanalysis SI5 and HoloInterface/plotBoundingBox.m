6a8513808b0c887e93c52fa4292b886a9c3d127f
amardinly <alan.mardinly@gmail.com>
2017-01-13 09:34:43 -0800

    Fixed plotBoundingBox to return outputs that can be used with 'rectangle' command.  Changed curvature on ROIanalysis rectangle display from 0.5 to 0.25

--- ROIanalysis SI5 and HoloInterface/plotBoundingBox.m
+++ ROIanalysis SI5 and HoloInterface/plotBoundingBox.m
@@ -44,74 +44,130 @@ load(fullPathToInterpolant)
 % XYZ_Calibration.EZ = 60000; % 40000; %Range in depth for testing purposes (This is the natural SLM coordinates)
 % 
 
+% Code Based On Nicos idea that doesnt work 
+% % SLMminX=min(XYZ_Points.GET.X);
+% % SLMmaxX=max(XYZ_Points.GET.X);
+% % SLMminY=min(XYZ_Points.GET.Y);
+% % SLMmaxY=max(XYZ_Points.GET.Y);
+% % 
+% % for j=1:numel(optotuneDepths)
+% % SLMPOINT=function_3DCofC([256 256 optotuneDepths(j)]',XYZ_Points);  %takes location in Real Units and Returns SLM units
+% % SLMZlevel(j)=SLMPOINT(3);
+% % end
+% % 
+% % 
+% % ReversePoints.ASK=XYZ_Points.GET;
+% % ReversePoints.GET=XYZ_Points.ASK;
+% % 
+% % for j = 1:numel(optotuneDepths)
+% % 
+% %     A=round(function_3DCofC([SLMminX SLMminY SLMZlevel(j)]',ReversePoints,5));  %takes location in Real Units and Returns SLM units
+% %     rect(j,1:2)=A(1:2);
+% %     
+% %     A=round(function_3DCofC([SLMmaxX SLMmaxY SLMZlevel(j)]',ReversePoints,5));  %takes location in Real Units and Returns SLM units
+% %     rect(j,3:4)=A(1:2); 
+% % end
 
-SLMminX=min(XYZ_Points.GET.X);
-SLMmaxX=max(XYZ_Points.GET.X);
-SLMminY=min(XYZ_Points.GET.Y);
-SLMmaxY=max(XYZ_Points.GET.Y);
+%code based on Alan's stupid Idea that does work
 
-
-
-
-for j=1:numel(optotuneDepths)
-    currentX=0;
-    currentY=0;
-    SLMPOINT=[0 0 0];
-    
-    while SLMPOINT(1)<SLMminX
-        
-        SLMPOINT=function_3DCofC([currentX currentY optotuneDepths(j)]',XYZ_Points);  %takes location in Real Units and Returns SLM units
-        currentX=currentX+1;
-        
-    end
-    
-    rect(j,1)=currentX;
-    
-    
-    currentY=0;
-    SLMPOINT=[0 0 0];
-    
-    while SLMPOINT(2)<SLMminY
-        
-        SLMPOINT=function_3DCofC([rect(1) currentY optotuneDepths(j)]',XYZ_Points);  %takes location in Real Units and Returns SLM units
-        %currentX=currentX+1;
-        currentY=currentY+1;
-        
-    end
-    
-    
-    rect(j,2)=currentY;
-    
-    
-    
-    
-    currentX=512;
-    currentY=512;
-    SLMPOINT=[1 1 1];
-    while SLMPOINT(1)>SLMmaxX
-        
-        SLMPOINT=function_3DCofC([currentX currentY optotuneDepths(j)]',XYZ_Points);  %takes location in Real Units and Returns SLM units
-        currentX=currentX-1;
-        
-    end
-    
-    rect(j,3)=currentX;
-    
-    
-    
-    currentY=512;
-    SLMPOINT=[1 1 1];
-    while SLMPOINT(2)>SLMmaxY
-        
-        SLMPOINT=function_3DCofC([currentX currentY optotuneDepths(j)]',XYZ_Points);  %takes location in Real Units and Returns SLM units
-        currentY=currentY-1;
-        
-    end
-    
-    rect(j,4)=currentY;
-    
-    
+U=[XYZ_Points.ASK.X XYZ_Points.ASK.Y XYZ_Points.ASK.Z];
+UniqZ=unique(U(:,3));
+for j = 1:numel(UniqZ);
+   thesePoints=U(find(U(:,3)==UniqZ(j)),1:2);
+   r(j,:)=[min(thesePoints(:,1)) min(thesePoints(:,2)) max(thesePoints(:,1)) max(thesePoints(:,2))];
 end
 
+for j = 1:numel(optotuneDepths)
+   [sorted indx]= sort(abs(UniqZ-optotuneDepths(j)));
+    
+   if optotuneDepths(j)>2;
+     A=1./r(indx(1),:);
+
+     B=1./r(indx(2),:);
+     
+     
+     %take inverse weighted average of neighboring planes above and below,
+     %the higher weight going to the closer plane
+     rect(j,:)=  1./  (mean([A*sorted(1); B*sorted(2)]) /  mean([sorted(1) sorted(2)]));
+     
+     
+       
+   else
+       
+       rect(j,:)=r(indx(1),:);
+       
+   end
+
+ 
+
+
+end
+
+
+rect(:,3:4)=rect(:,3:4)-rect(:,1:2);
+
+
+%Cdoe based on a REALLY stupid Alan idea that DOES Work
+
+% %for j=1:numel(optotuneDepths)
+%     currentX=0;
+%     currentY=0;
+%     SLMPOINT=[0 0 0];
+%     
+%     while SLMPOINT(1)<SLMminX
+%         
+%         SLMPOINT=function_3DCofC([currentX currentY optotuneDepths(j)]',XYZ_Points);  %takes location in Real Units and Returns SLM units
+%         currentX=currentX+1;
+%         
+%     end
+%     
+%     rect(j,1)=currentX;
+%     
+%     
+%     currentY=0;
+%     SLMPOINT=[0 0 0];
+%     
+%     while SLMPOINT(2)<SLMminY
+%         
+%         SLMPOINT=function_3DCofC([rect(1) currentY optotuneDepths(j)]',XYZ_Points);  %takes location in Real Units and Returns SLM units
+%         %currentX=currentX+1;
+%         currentY=currentY+1;
+%         
+%     end
+%     
+%     
+%     rect(j,2)=currentY;
+%     
+%     
+%     
+%     
+%     currentX=512;
+%     currentY=512;
+%     SLMPOINT=[1 1 1];
+%     while SLMPOINT(1)>SLMmaxX
+%         
+%         SLMPOINT=function_3DCofC([currentX currentY optotuneDepths(j)]',XYZ_Points);  %takes location in Real Units and Returns SLM units
+%         currentX=currentX-1;
+%         
+%     end
+%     
+%     rect(j,3)=currentX;
+%     
+%     
+%     
+%     currentY=512;
+%     SLMPOINT=[1 1 1];
+%     while SLMPOINT(2)>SLMmaxY
+%         
+%         SLMPOINT=function_3DCofC([currentX currentY optotuneDepths(j)]',XYZ_Points);  %takes location in Real Units and Returns SLM units
+%         currentY=currentY-1;
+%         
+%     end
+%     
+%     rect(j,4)=currentY;
+%     
+%     
+% end
+% 
 
 
